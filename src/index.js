cd915a3f8e97dfed7033481246b81141f53408b4
Tony Nguyen <huy5850@gmail.com>
2024-11-19 14:00:57 -0500

    add analyst

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import Books from './components/Book';
 import 'primereact/resources/themes/saga-blue/theme.css';   /* theme */
 import 'primereact/resources/primereact.min.css';           /* core css */
 import 'primeicons/primeicons.css';                         /* icons */
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Books />
+    <Analytics />
   </React.StrictMode>
 );
 
